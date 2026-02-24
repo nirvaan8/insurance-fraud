0c5595126e2f1daae2656568a3082ffa41e9f46a
Muskan <muskan.23@st.niituniversity.in>
2026-02-24 21:30:08 +0530

    login javascript

---
+++ frontend/login.js
@@ -0,0 +1,45 @@
+async function login() {
+
+  console.log("Login clicked");
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (!email || !password) {
+    alert("Please fill all fields ❌");
+    return;
+  }
+
+  try {
+    console.log("Sending request...");
+
+    const res = await fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email, password })
+    });
+
+    const data = await res.json();
+
+    console.log("Response:", data);
+
+    if (res.ok) {
+      localStorage.setItem("role", data.role);
+      alert("Login Successful ✅");
+      window.location.href = "index.html";
+    } else {
+      alert(data.error || "Login failed ❌");
+    }
+
+  } catch (err) {
+    console.error("ERROR:", err);
+    alert("Server error ❌");
+  }
+}
+
+/* GOOGLE LOGIN */
+function googleLogin() {
+  window.location.href = "http://localhost:3000/auth/google";
+}
