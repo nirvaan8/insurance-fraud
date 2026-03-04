90c24c61825c7a7109127ca95f40b740654247af
nirvaan8 <87943615+nirvaan8@users.noreply.github.com>
2026-03-05 01:45:47 +0530

    Change server port from 3000 to 8080

--- backend/server.js
+++ backend/server.js
@@ -552,5 +552,5 @@ app.post("/upload", upload.single("file"), async (req, res) => {
 });
 
 /* ================= START ================= */
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
